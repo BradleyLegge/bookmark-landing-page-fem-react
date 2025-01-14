1191af9c75c9aa2739448afe417f35c0043ed4ff
BradleyLegge <bmlegge12@gmail.com>
2025-01-13 21:05:50 -0500

    Added css to Faq component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,18 +5,64 @@ const Faq = () => {
   return (
     <div className="faq-hero">
       <h2>Frequently Asked Questions</h2>
-      <p>
+      <p className="faq-para">
         Here are some of our FAQs. If you have any other questions you'd like
         answered please feel free to email us.
       </p>
+      <div className="line"></div>
       <div className="faq-container">
-        <div className="line"></div>
-        <div className="question-container">
-          <div className="question">What is Bookmark?</div>
-          <img src={downArrow} alt="Image of down arrow" />
+        <button className="qa-btn">
+          <p>What is Bookmark?</p>
+          <img src={downArrow} alt="Down Arrow" />
+        </button>
+        <div className="ans-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            culpa nemo ullam praesentium unde nulla.
+          </p>
         </div>
-        <div className="line"></div>
       </div>
+      <div className="line"></div>
+      <div className="faq-container">
+        <button className="qa-btn">
+          <p>How can I request a new browser?</p>
+          <img src={downArrow} alt="Down Arrow" />
+        </button>
+        <div className="ans-container">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum,
+            earum.
+          </p>
+        </div>
+      </div>
+      <div className="line"></div>
+      <div className="faq-container">
+        <button className="qa-btn">
+          <p>Is there a mobile app?</p>
+          <img src={downArrow} alt="Down Arrow" />
+        </button>
+        <div className="ans-container">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            molestiae repellendus dolorem minima vel porro voluptas ipsa eum
+            illum perferendis?
+          </p>
+        </div>
+      </div>
+      <div className="line"></div>
+      <div className="faq-container">
+        <button className="qa-btn">
+          <p>What about other Chromium browsers?</p>
+          <img src={downArrow} alt="Down Arrow" />
+        </button>
+        <div className="ans-container">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+            aliquam.
+          </p>
+        </div>
+      </div>
+      <div className="line"></div>
       <button className="faq-btn">More Info</button>
     </div>
   );
